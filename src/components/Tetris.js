b85a8de29f885b158261058a7d0954fa6b920755
ibrahim AKIN <ibrahim-akin@msn.com>
2020-10-09 23:55:54 +0300

    init

---
+++ src/components/Tetris.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import Stage from './Stage';
+import Display from './Display';
+import StartButton from './StartButton';
+// Styled Components
+import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
+// Custom Hooks
+import { usePlayer } from '../hooks/usePlayer';
+import { useStage } from '../hooks/useStage';
+
+const Tetris = (props) => {
+
+    const [dropTime, setDropTime] = useState(null);
+    const [gameOver, setGameOver] = useState(false);
+
+    const [player] = usePlayer();
+    const [stage, setStage] = useStage(player);
+
+    return (
+        <StyledTetrisWrapper>
+            <StyledTetris>
+                <Stage stage={stage} />
+                <aside>
+                    {gameOver ?
+                        (
+                            <Display gameOver={gameOver} text="Game Over" />
+                        )
+                        :
+                        (
+                            <div>
+                                <Display text="Score" />
+                                <Display text="Rows" />
+                                <Display text="Level" />
+                            </div>
+                        )
+                    }
+                    <StartButton />
+                </aside>
+            </StyledTetris>
+        </StyledTetrisWrapper>
+    );
+}
+
+export default Tetris;
