4819c97967ff4a751d559783d9807074ab061213
ibrahim AKIN <ibrahim-akin@msn.com>
2020-10-10 20:00:20 +0300

    update collision detection

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -2,10 +2,8 @@ import React from 'react';
 import { StyledCell } from './styles/StyledCell';
 import { TETROMINOS } from '../tetrominos';
 
-const Cell = ({ type }) => {
-    return (
-        <StyledCell type={type} color={TETROMINOS[type].color} />
-    );
-}
+const Cell = ({ type }) => (
+  <StyledCell type={type} color={TETROMINOS[type].color} />
+)
 
 export default Cell;
