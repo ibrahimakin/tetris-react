c8829d8ae8f8c3f0451e56989a8a11b9e03537f9
ibrahim AKIN <ibrahim-akin@msn.com>
2021-04-05 22:56:50 +0300

    Update

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -13,25 +13,26 @@ import { useGameStatus } from '../hooks/useGameStatus';
 
 // Components
 import Stage from './Stage';
-import Display from './Display';
-import StartButton from './StartButton';
 
 const Tetris = (props) => {
   const [dropTime, setDropTime] = useState(null);
   const [gameOver, setGameOver] = useState(false);
+  const [started, setStarted] = useState(false);
+  const [paused, setPaused] = useState(false);
 
   const [player, updatePlayerPos, resetPlayer, playerRotate] = usePlayer();
   const [stage, setStage, rowsCleared] = useStage(player, resetPlayer);
   const [score, setScore, rows, setRows, level, setLevel, highScore, maxRows] = useGameStatus(rowsCleared);
 
   const movePlayer = dir => {
-    if (!checkCollision(player, stage, { x: dir, y: 0 })) {
+    if (!paused && !checkCollision(player, stage, { x: dir, y: 0 })) {
       updatePlayerPos({ x: dir, y: 0 });
     }
   };
 
   const startGame = () => {
     // Reset everything
+    setStarted(true);
     setStage(createStage());
     setDropTime(1000);
     resetPlayer();
@@ -41,6 +42,14 @@ const Tetris = (props) => {
     setLevel(0);
   };
 
+  const handleResume = () => {
+    setPaused(false);
+  }
+
+  const handlePause = () => {
+    setPaused(true);
+  }
+
   const drop = () => {
     // Increase level when player has cleared 10 rows
     if (rows > (level + 1) * 10) {
@@ -49,16 +58,19 @@ const Tetris = (props) => {
       setDropTime(1000 / (level + 1) + 200);
     }
 
-    if (!checkCollision(player, stage, { x: 0, y: 1 })) {
-      updatePlayerPos({ x: 0, y: 1, collided: false });
-    } else {
-      // Game Over
-      if (player.pos.y < 1) {
-        //console.log('GAME OVER!!!');
-        setGameOver(true);
-        setDropTime(null);
+    if (!paused) {
+      if (!checkCollision(player, stage, { x: 0, y: 1 })) {
+        updatePlayerPos({ x: 0, y: 1, collided: false });
+      } else {
+        // Game Over
+        if (player.pos.y < 1) {
+          //console.log('GAME OVER!!!');
+          setGameOver(true);
+          setStarted(false);
+          setDropTime(null);
+        }
+        updatePlayerPos({ x: 0, y: 0, collided: true });
       }
-      updatePlayerPos({ x: 0, y: 0, collided: true });
     }
   };
 
@@ -86,7 +98,9 @@ const Tetris = (props) => {
       } else if (keyCode === 40) {
         dropPlayer();
       } else if (keyCode === 38) {
-        playerRotate(stage, 1);
+        if (!paused) {
+          playerRotate(stage, 1);
+        }
       }
     }
   };
@@ -108,17 +122,21 @@ const Tetris = (props) => {
         </div>
         <aside>
           { gameOver ? (
-            <Display gameOver={gameOver} text={'game_over'} />
+            <div className="styled-display gameover"><span lang-tag="game_over">Game Over</span></div>
           ) : (
             <div>
-              <Display text={`${'best_score'}: ${highScore}`} />
-              <Display text={`${'score'}: ${score}`} />
-              <Display text={`${'max_rows'}: ${maxRows}`} />
-              <Display text={`${'rows'}: ${rows}`} />
-              <Display text={`${'level'}: ${level}`} />
+              <div className="styled-display"><span lang-tag="high_score">High Score</span>{`: ${highScore}`}</div>
+              <div className="styled-display"><span lang-tag="score">Score</span>{`: ${score}`}</div>
+              <div className="styled-display"><span lang-tag="max_rows">Max Rows</span>{`: ${maxRows}`}</div>
+              <div className="styled-display"><span lang-tag="rows">Rows</span>{`: ${rows}`}</div>
+              <div className="styled-display"><span lang-tag="level">Level</span>{`: ${level}`}</div>
             </div>
           )}
-          <StartButton callback={startGame} text={'start_game'} />
+          <button className="styled-button" onClick={startGame}><span lang-tag="start_game">Start Game</span></button>
+          { started ? (paused ?
+            <button className="styled-button" onClick={handleResume}><span lang-tag="resume_game">Resume Game</span></button>:
+            <button className="styled-button" onClick={handlePause}><span lang-tag="pause_game">Pause Game</span></button>) : null
+          }
         </aside>
       </StyledTetris>
     </StyledTetrisWrapper>
