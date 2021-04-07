9e02e071aba93569b1ff256325c1f3e510e33271
ibrahim AKIN <ibrahim-akin@msn.com>
2021-04-07 23:14:01 +0300

    Update Lang

--- src/lang/index.js
+++ src/lang/index.js
@@ -3,7 +3,7 @@ export const langObjTetris = {
         score: 'Skor',
         rows: 'Rows',
         level: 'Seviye',
-        start_game: 'Oyuna Basla',
+        start_game: 'Basla',
         game_over: 'Oyun Bitti',
         high_score: 'En Yüksek Skor',
         max_rows: 'Max Rows',
