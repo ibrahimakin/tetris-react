c1a02fb2c935c84670ced1a421fcc7b0b5b761b0
Vestel <ibrahim-akin@msn.com>
2023-10-08 15:20:31 +0300

    refactoring

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,6 +1,15 @@
 import React from 'react';
-import { StyledCell } from './styles/StyledCell';
 import { TETROMINOS } from '../helper';
+import styled from 'styled-components';
+
+const StyledCell = styled.div`
+    background-color: rgba(${props => props.color}, .8);
+    border: ${props => (props.type === 0 ? '0px solid' : '4px solid')};
+    border-bottom-color: rgba(${props => props.color}, .1);
+    border-right-color: rgba(${props => props.color}, 1);
+    border-left-color: rgba(${props => props.color}, .3);
+    border-top-color: rgba(${props => props.color}, 1);
+`;
 
 const Cell = ({ type }) => <StyledCell type={type} color={TETROMINOS[type].color} />;
 
--- src/index.js
+++ src/index.js
@@ -10,3 +10,27 @@ ReactDOM.render(<Tetris />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+/*
+import { lang_tetris as lang_obj } from './lang';
+const buttons = [];
+const langs = ['tr', 'en'];
+for (let i = 0; i < 2; i++) {
+    buttons.push(document.createElement('button'));
+    buttons[i].setAttribute('lang-name', langs[i]);
+    buttons[i].onclick = event => {
+        let lang = event.target.getAttribute('lang-name');
+        try { localStorage.setItem('lang', lang); } catch (e) { }
+        for (const element of document.querySelectorAll('[lang-tag]')) {
+            let value = lang_obj[lang][element.getAttribute('lang-tag')]
+            if (element.placeholder) element.placeholder = value;
+            else element.textContent = value;
+        }
+    };
+    buttons[i].innerText = langs[i];
+    buttons[i].style.zIndex = 3;
+    buttons[i].style.bottom = 0;
+    buttons[i].style.position = 'fixed';
+    buttons[i].style.left = 70 * i + 'px';
+    document.body.appendChild(buttons[i]);
+}
+*/
